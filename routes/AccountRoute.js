00ccc65ad66888fbff02f2770dd1250e2a31972a
Daniel Owusu <owusudaniel077@gmail.com>
2025-04-06 17:39:46 +0000

    A simple bank management system with CRUD operations

--- routes/AccountRoute.js
+++ routes/AccountRoute.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   createAccountDetails,
   retrieveAccountDetails,
+  updateAccountDetails,
 } = require("../controller/AccountController");
 
 const router = express.Router();
 
 router.post("/account", createAccountDetails);
 router.get("/account", retrieveAccountDetails);
+router.put("/account", updateAccountDetails);
 
 module.exports = router;
